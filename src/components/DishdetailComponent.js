ca34e2fbdce63fad9632b8b7b98915e8c5a26e9e
Mouslim Aitssi <mouslimaitssi@gmail.com>
2020-10-15 20:00:18 +0100

    fetch from server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { Card, CardImg, CardTitle, CardBody, CardText , BreadcrumbItem, Breadcrumb, Button} from "reactstrap";
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 import CommentForm from './CommentFormComponent';
 
@@ -105,7 +106,7 @@ import CommentForm from './CommentFormComponent';
         return(
             <div className="col-12 col-md-5 m-1">
                 <Card>
-                    <CardImg width="100%" src= {dish.image} alt= {dish.name}/>
+                    <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                     <CardBody>
                         <CardTitle>{dish.name}</CardTitle>
                         <CardText>
